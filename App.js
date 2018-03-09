f25fcaa285da310e1717ee2dcb0deb256670d7f8
github username <github email address>
2018-03-08 22:27:58 -0800

    testing

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+import Header from './src/components/header';
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <Header />
+    );
+  }
+}
+
