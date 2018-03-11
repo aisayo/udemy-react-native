d9854d9ec8ff07637c1a15120cdf24dd9bf0ad76
github username <github email address>
2018-03-10 23:13:59 -0800

    added thumbnail to albumdetail component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardItem from './CardItem';
 
@@ -7,15 +7,38 @@ import CardItem from './CardItem';
 //presentational component
 
 //(props) is receiving the props being passed down
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image} = album;
+    const { thumbnailStyle, headerContentStyle } = styles;
     return (
         <Card>
             <CardItem>
-                <Text>{props.album.title}</Text>
+                <View>
+                    <Image 
+                        style={thumbnailStyle}
+                        source={{ uri: thumbnail_image }} 
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
             </CardItem>
         </Card>
        
     );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    }
 };
 
 export default AlbumDetail;
