d6e801a90a7bb216a0fa41075e6c7eecffd3baee
github username <github email address>
2018-03-10 17:18:30 -0800

    set state for albumlist component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-class AlbumList extends Component {
+//state is a plain js object that is available to class baeed components only
+//used to record and respond to user-triggers events in component
+//when we need to update what a component shows we are going to call setState
+//only use this.setState, never use this.state
 
+
+//props is for parent to child communication
+
+class AlbumList extends Component { //this is a class based component
+
+    //componentWillMount will run when app first gets launched. before anything
+    //is rendered
     componentWillMount() {
-        console.log('componentWillMount in AlbumList')
+        //declare an initial state 
+        state = { albums: [] };
+        //fetching data
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            //this is the request handler
+            .then(response => this.setState({albums: response.data})); //fetching list of albums
     }
     render() {
+        console.log(this.state)
         return (
             <View>
                 <Text>Album List!!!</Text>
