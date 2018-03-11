e216985fa2c65ed91b90bbbdbaf535588897ad05
github username <github email address>
2018-03-10 23:25:24 -0800

    added image component to albumdetail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -8,21 +8,26 @@ import CardItem from './CardItem';
 
 //(props) is receiving the props being passed down
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image} = album;
-    const { thumbnailStyle, headerContentStyle } = styles;
+    const { title, artist, thumbnail_image, image} = album;
+    const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
     return (
         <Card>
             <CardItem>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image 
                         style={thumbnailStyle}
                         source={{ uri: thumbnail_image }} 
                     />
                 </View>
                 <View style={headerContentStyle}>
-                    <Text>{title}</Text>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardItem>
+            <CardItem>
+                <Image
+                style={imageStyle} 
+                source={{ uri: image }}/>
             </CardItem>
         </Card>
        
@@ -35,9 +40,23 @@ const styles = {
         justifyContent: 'space-around'
 
     },
+    headerTextStyle: {
+        fontSize: 18
+    },
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 };
 
